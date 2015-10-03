340312d9249dbf0ee5617b0e906436bb042c8989
threefure <threefure@gmail.com>
2015-10-02 22:02:12 -0700

    initial commit for hello world

---
+++ src/com/bananaplan/constructioncase/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.bananaplan.constructioncase;
+
+public class HelloWorld {
+	private String message;
+
+	   public void setMessage(String message){
+	      this.message  = message;
+	   }
+
+	   public void getMessage(){
+	      System.out.println("Your Message : " + message);
+	   }
+}
